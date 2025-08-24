d5fbcb7bf32785a3e6be76c3a687d8a4a5ecc385
Michaël Besily <Michael.besily@gmail.com>
2025-08-24 22:03:51 +0100

    update index and jacpot view

--- public/urss.js
+++ public/urss.js
@@ -1,8 +1,48 @@
+const liensChelous = [
+  "https://www.yandex.ru/",
+  "http://www.404page.ru/",
+  "https://www.kulturologia.ru/",
+  "https://www.komarovskiy.net/",
+  "http://www.pikabu.ru/",
+
+  "https://www.kaskus.co.id/",
+  "https://www.klikindomaret.com/",
+  "https://www.bola.com/",
+  "https://www.jpnn.com/",
+  "https://theuselessweb.com/",
+  "http://pointerpointer.com/",
+  "https://www.staggeringbeauty.com/",
+  "https://www.patience-is-a-virtue.org/",
+  "https://www.koalastothemax.com/",
+  "https://www.zoomquilt.org/",
+  "http://www.windows93.net/",
+  "https://fakeupdate.net/",
+  "https://www.trypap.com/",
+  "https://north-verification.com/notif.html?an=at&cid=57687907c3c6082ad3d714afe91422e3&sid=26578321",
+  "https://heroadblocker.pro/ext.php?ah=dgtr17hUtCI&ao=26578321&ak=5764133fdf643d66348c200bf600e637&aj=tr&an=2&am=1164916",
+  "https://neollowerii.com/click.php?key=d2i8ifl92fb002n660b0&type=TBI&age=&creative_id=&campaign_id=&site_id=18746&placement_id=61914085&preset_id=&click_id=5768382c61838974a32fcb1c8023805f&entity_placement_id=61713",
+  "https://siltable.com/click.php?key=d2ivck592fb0024prrfg&type=TBI&age=&creative_id=&campaign_id=&site_id=18941&placement_id=62010275&preset_id=&click_id=5767fae35870a7beaa936dc0f3446dca&entity_placement_id=61713",
+  "https://lakns.com/afu.php?zoneid=5593108&var=5593108&rid=ksX-wKK1z8yLZCaWKyzJyw%3D%3D&rhd=false&ab2r=0&sf=1&os=windows&os_version=19.0.0&is_mobile=false&browser_version=130.0.6723.117",
+  "https://lakns.com/afu.php?zoneid=5593108&var=5593108&rid=ksX-wKK1z8yLZCaWKyzJyw%3D%3D&rhd=false&ab2r=0&sf=1&os=windows&os_version=19.0.0&is_mobile=false&browser_version=130.0.6723.117"
+];
+
+let compteurClic = 0;
+
 document.getElementById('jackpot-btn').onclick = () => {
-  const paiementModal = new bootstrap.Modal(document.getElementById('paiementModal'));
-  paiementModal.show();
-  document.getElementById('paiement-feedback').textContent = "";
-  document.getElementById('fake-paiement-form').reset();
+  compteurClic++;
+  if (compteurClic % 5 === 0) {
+    // On peut afficher la vraie popup en plus si besoin :
+    const paiementModal = new bootstrap.Modal(document.getElementById('paiementModal'));
+    paiementModal.show();
+    document.getElementById('paiement-feedback').textContent = "";
+    document.getElementById('fake-paiement-form').reset();
+    compteurClic = 0; // Remise à zéro après la popup/pub
+  } else {
+    const url = liensChelous[Math.floor(Math.random() * liensChelous.length)];
+    window.open(url, '_blank');
+    document.getElementById('result').innerHTML =
+      `<div class="alert alert-warning mt-3">Encore ${3 - (compteurClic % 3)} clique${3 - (compteurClic % 3) === 1 ? '' : 's'} avant la vraie chance et... une surprise !</div>`;
+  }
 };
 
 document.getElementById('fake-paiement-form').onsubmit = function(e) {
@@ -21,34 +61,8 @@ document.getElementById('fake-paiement-form').onsubmit = function(e) {
     feedback.style.color = "green";
     setTimeout(() => {
       bootstrap.Modal.getInstance(document.getElementById('paiementModal')).hide();
-      fetch('/jackpot')
-        .then(res => res.json())
-        .then(data => {
-          // Affiche les jeux tirés avec image et clé
-          document.body.innerHTML = `
-            <div class="container mt-5">
-              <h2 class="mb-4">Voici tes jeux camarade :</h2>
-              <div class="row">
-                ${data.jeux.map(j => `
-                  <div class="col-md-4 mb-4">
-                    <div class="card h-100">
-                      <img src="${j.image}" class="card-img-top" alt="${j.nom}">
-                      <div class="card-body">
-                        <h5 class="card-title">${j.nom}</h5>
-                        <p class="card-text"><strong>Clé Steam :</strong> ${j.cle || "Non disponible"}</p>
-                      </div>
-                    </div>
-                  </div>
-                `).join('')}
-              </div>
-              <a href="/" class="btn btn-danger mt-4">Revenir à l'accueil</a>
-            </div>
-          `;
-        })
-        .catch(err => {
-          document.body.innerHTML = "<div class='alert alert-danger'>Erreur serveur : impossible de récupérer les jeux.</div>";
-          console.error("Erreur côté client : ", err);
-        });
+      // Redirection réelle vers /jackpot
+      window.location.href = '/jackpot';
     }, 1100);
   } else {
     feedback.textContent = "Échec du paiement.";
